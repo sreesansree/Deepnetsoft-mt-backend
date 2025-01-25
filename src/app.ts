3eb72f0db5778ff9ddcc5f273428dd32fe338b95
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-01-26 02:16:04 +0530

    Cors error fixed

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,18 @@ import meniItemRoutes from "./routes/menuItemRoutes"
 
 const app = express()
 
+// Enable CORS for specific origin
+const corsOptions = {
+    origin: "http://localhost:5173", // Allow frontend to make requests
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],             // Allowed HTTP methods
+    allowedHeaders: "Content-Type",  // Allowed headers
+};
+
+
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors(corsOptions))
+app.use(express.json()) // Parse incoming JSON requests
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
 /* Routes */
 app.use("/api/menus", menuRoutes);
--- src/routes/menuItemRoutes.ts
+++ src/routes/menuItemRoutes.ts
@@ -3,7 +3,9 @@ import { getMenuItems, createMenuItem } from "../controllers/menuItemController"
 
 const router = express.Router()
 
-router.get("/:menuId", getMenuItems)
-router.post("/", createMenuItem)
+// Fetch menu items for a specific menu
+router.get("/:menuId/items", getMenuItems)
+// Create a new menu item for a specific menu
+router.post("/:menuId/items", createMenuItem)
 
 export default router
