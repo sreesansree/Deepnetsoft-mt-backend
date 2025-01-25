8fb805fd012458cafc6bc2038371d87feb8622b9
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-01-25 16:05:26 +0530

    Added Uniq name validation for menuitem

--- src/models/MenuItem.ts
+++ src/models/MenuItem.ts
@@ -7,10 +7,36 @@ export interface IMenuItem extends Document {
     price: number
 }
 const MenuItemSchema: Schema = new Schema({
-    menuId: { type: mongoose.Schema.Types.ObjectId, ref: "Menu", required: true },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
+    menuId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Menu",
+        required: true
+    },
+    name: {
+        type: String,
+        required: [true, "Item name is required"],
+        trim: true,
+        minlength: [3, "Item name must be at least 3 characters long"],
+        maxlength: [50, "Item name must not exceed 50 characters"]
+
+    },
+    description: {
+        type: String,
+        required: [true, "Item description is required"],
+        trim: true,
+        minlength: [5, "Item description must be at least 5 characters long"],
+        maxlength: [200, "Item description must not exceed 200 characters"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"],
+        min: [0, "Price must be a positive number"],
+    },
 })
+
+// Compound index to enforce uniqueness of `name` within the same `menuId`
+
+MenuItemSchema.index({ menuId: 1, name: 1 }, { unique: true });
+
 
 export default mongoose.model<IMenuItem>("MenuItem", MenuItemSchema)
